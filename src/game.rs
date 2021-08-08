f5d3116f327c6d92e67bb3bc63cfd75e1c38ac7c
Daniel Johnson <danieljohnson2@fastmail.com>
2021-08-08 10:11:30 -0400

    Commenting

--- src/game.rs
+++ src/game.rs
@@ -35,15 +35,19 @@ impl Game {
         GameRef::new(RefCell::new(self))
     }
 
+    /// Provides an immutable reference to the board.
     pub fn to_board(&self) -> &Board {
         &self.board
     }
 
-    pub fn has_any_moves(&mut self, cell: Cell) -> bool {
-        let board = &self.board;
-        !board.find_valid_moves(cell).is_empty()
-    }
-
+    /// Checks the game staet and returns the cell of the player
+    /// whose move it now is. This will also apply ongoing moves, so it
+    /// can change the game state. If there are any ongoing moves pending
+    /// when it returns, it returns Empty- nobody can move until outstanding
+    /// moves clear.
+    /// 
+    /// Outsanding moves run on a timer; this may do nothing (and return Empty)
+    /// if there are outstanding moves that are not yet due.
     pub fn check_move(&mut self) -> Cell {
         if self.ongoing_movement.is_valid() {
             let now = Instant::now();
@@ -57,7 +61,7 @@ impl Game {
                 if !self.ongoing_movement.is_valid() {
                     let f = self.next_move.flipped();
 
-                    if self.has_any_moves(f) {
+                    if !self.board.find_valid_moves(f).is_empty() {
                         self.next_move = f;
                     }
                 }
@@ -71,6 +75,9 @@ impl Game {
         }
     }
 
+    /// Constructs a movement for a move at the location indicated. This
+    /// can result in an invalid move, if 'loc' is not a valid location
+    /// or it is not anyone's turn.
     pub fn get_player_movement(&self, loc: Loc) -> Movement {
         if self.next_move != Cell::Empty {
             Movement::new(&self.board, loc, self.next_move)
@@ -79,6 +86,8 @@ impl Game {
         }
     }
 
+    /// Constructs a movement for the current player. It may be an invalid
+    /// move if is not anyone's turn, or the current player has no valid moves.
     pub fn get_ai_movement(&self) -> Movement {
         if self.next_move != Cell::Empty {
             let valid = self.board.find_valid_moves(self.next_move);
@@ -88,6 +97,11 @@ impl Game {
         }
     }
 
+    /// This plays a move. The move will play out over time, and
+    /// it will switch to a new player's turn only when complete.Duration
+    /// 
+    /// This returns false if the movement is invalid, or if another
+    /// movement is ongoing. In this case no new movement is begun,
     pub fn begin_movement(&mut self, mv: Movement) -> bool {
         if self.ongoing_movement.is_valid() {
             false
@@ -99,9 +113,12 @@ impl Game {
         }
     }
 
+    /// This plays a move, like begin_movement. This will also speed the move
+    /// up slightly so the first flip will occur at once.
     pub fn begin_immediate_movement(&mut self, mv: Movement) -> bool {
         if self.begin_movement(mv) {
             self.next_move_time = Instant::now();
+            self.check_move();
             true
         } else {
             false
